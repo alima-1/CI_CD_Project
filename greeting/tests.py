e8865ce633480913e3caed6e8efacd0ef0c57bce
alima <alima.ayikoru1@gmail.com>
2025-04-18 09:51:52 +0300

    Added tests to work

--- greeting/tests.py
+++ greeting/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class GreeetingTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response =  self.client.get(reverse('index'))#refrencing url
+        self.assertEqual(response.status_code, 200)#expecting response 200
+        self.assertContains(response,'Hello')#there should be a word hello
+        self.assertTemplateUsed(response,'index.html')#is the page called index.html
+ 
+class GreetingFunctionalityTest(TestCase):
+    def test_greeting(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.context['greeting'], 'Hello')
